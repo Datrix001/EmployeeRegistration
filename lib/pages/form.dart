0a6873cbaa57d2feadfd32564599012ad6e1b059
Datrix001 <mayankmoullisingh@gmail.com>
2025-02-21 01:35:05 +0530

    Add feautre added successfully

--- lib/pages/form.dart
+++ lib/pages/form.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:random_string/random_string.dart';
+
+import '../service/database.dart';
 
 class form extends StatefulWidget {
   const form({super.key});
@@ -8,6 +12,10 @@ class form extends StatefulWidget {
 }
 
 class _formState extends State<form> {
+  TextEditingController namecontroller = TextEditingController();
+  TextEditingController agecontroller = TextEditingController();
+  TextEditingController locationcontroller = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -33,7 +41,7 @@ class _formState extends State<form> {
         ),
       ),
       body: Container(
-        margin: EdgeInsets.only(left: 20, top: 30),
+        margin: EdgeInsets.only(left: 20, top: 30, right: 20),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
@@ -53,6 +61,7 @@ class _formState extends State<form> {
                   border: Border.all(),
                   borderRadius: BorderRadius.circular(10)),
               child: TextField(
+                controller: namecontroller,
                 decoration: InputDecoration(border: InputBorder.none),
               ),
             ),
@@ -75,6 +84,7 @@ class _formState extends State<form> {
                   border: Border.all(),
                   borderRadius: BorderRadius.circular(10)),
               child: TextField(
+                controller: agecontroller,
                 decoration: InputDecoration(border: InputBorder.none),
               ),
             ),
@@ -97,21 +107,41 @@ class _formState extends State<form> {
                   border: Border.all(),
                   borderRadius: BorderRadius.circular(10)),
               child: TextField(
+                controller: locationcontroller,
                 decoration: InputDecoration(border: InputBorder.none),
               ),
             ),
             SizedBox(
               height: 30,
             ),
-            
             Center(
               child: ElevatedButton(
-                  onPressed: () => {},
+                  onPressed: () async {
+                    String Id = randomAlphaNumeric(10);
+                    Map<String, dynamic> employeeInfoMap = {
+                      "name": namecontroller.text,
+                      "age": agecontroller.text,
+                      "location": locationcontroller.text
+                    };
+                    await Database()
+                        .addEmployeeDetails(employeeInfoMap, Id)
+                        .then((value) {
+                      Fluttertoast.showToast(
+                          msg: "Employee Details Added Successfully",
+                          toastLength: Toast.LENGTH_SHORT,
+                          gravity: ToastGravity.CENTER,
+                          timeInSecForIosWeb: 1,
+                          backgroundColor: Colors.red,
+                          textColor: Colors.white,
+                          fontSize: 16.0);
+                    });
+                  },
                   child: Text(
                     "Submit",
-                    style: TextStyle(color: Colors.black,
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold),
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold),
                   )),
             )
           ],
