8a1bc2f58e8a3127b7ccf9825bb3787c482c5844
Datrix001 <mayankmoullisingh@gmail.com>
2025-02-28 07:00:37 +0530

    Minor tweaks to UI(Sized Box)

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,4 +1,3 @@
-import 'dart:math';
 
 import 'package:flutter/material.dart';
 import 'package:notes/pages/form.dart';
@@ -15,31 +14,31 @@ class _HomepageState extends State<Homepage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      floatingActionButton: FloatingActionButton(
-          onPressed: () => Navigator.pushNamed(context, "/form"),
-          child: Icon(Icons.add)),
-      appBar: AppBar(
-        title: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              "Flutter",
-              style: TextStyle(
-                  fontSize: 24,
-                  color: Colors.blue,
-                  fontWeight: FontWeight.bold),
-            ),
-            Text(
-              "Firebase",
-              style: TextStyle(
-                  fontSize: 24,
-                  color: Colors.orange,
-                  fontWeight: FontWeight.bold),
-            )
-          ],
+        floatingActionButton: FloatingActionButton(
+            onPressed: () => Navigator.pushNamed(context, "/form"),
+            child: Icon(Icons.add)),
+        appBar: AppBar(
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "Flutter",
+                style: TextStyle(
+                    fontSize: 24,
+                    color: Colors.blue,
+                    fontWeight: FontWeight.bold),
+              ),
+              Text(
+                "Firebase",
+                style: TextStyle(
+                    fontSize: 24,
+                    color: Colors.orange,
+                    fontWeight: FontWeight.bold),
+              )
+            ],
+          ),
         ),
-      ),
-      body: Container(
+        body: Container(
           margin: EdgeInsets.symmetric(horizontal: 20, vertical: 30),
           child: StreamBuilder<List<User>>(
               stream: readUser(),
@@ -59,54 +58,60 @@ class _HomepageState extends State<Homepage> {
                 final users = snapshot.data!;
 
                 return ListView.builder(
-              itemCount: users.length,
-              itemBuilder: (context, index) {
-                final user = users[index];
+                    itemCount: users.length,
+                    itemBuilder: (context, index) {
+                      final user = users[index];
 
-                return Material(
-                  elevation: 5,
-                  borderRadius: BorderRadius.circular(10),
-                  child: Container(
-                    padding: EdgeInsets.all(20),
-                    width: MediaQuery.of(context).size.width,
-                    decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.circular(20)),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children:[
-                          Text(
-                            "Name : ${user.name}",
-                            style: TextStyle(
-                                color: Colors.blue,
-                                fontSize: 20,
-                                fontWeight: FontWeight.bold),
+                      return Column(
+                        children: [Material(
+                          elevation: 5,
+                          borderRadius: BorderRadius.circular(10),
+                          child: Container(
+                            padding: EdgeInsets.all(20),
+                            width: MediaQuery.of(context).size.width,
+                            decoration: BoxDecoration(
+                                color: Colors.white,
+                                borderRadius: BorderRadius.circular(20)),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceBetween,
+                                    children: [
+                                      Text(
+                                        "Name : ${user.name}",
+                                        style: TextStyle(
+                                            color: Colors.blue,
+                                            fontSize: 20,
+                                            fontWeight: FontWeight.bold),
+                                      ),
+                                      Icon(Icons.edit)
+                                    ]),
+                                Text(
+                                  "Age : ${user.age}",
+                                  style: TextStyle(
+                                      color: Colors.orange,
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                                Text(
+                                  "Location : ${user.location}",
+                                  style: TextStyle(
+                                      color: Colors.blue,
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                              ],
+                            ),
                           ),
-                          Icon(Icons.edit)
-                      ]),
-                        Text(
-                          "Age : ${[user.age].toString()}",
-                          style: TextStyle(
-                              color: Colors.orange,
-                              fontSize: 20,
-                              fontWeight: FontWeight.bold),
                         ),
-                        Text(
-                          "Location : ${user.location}",
-                          style: TextStyle(
-                              color: Colors.blue,
-                              fontSize: 20,
-                              fontWeight: FontWeight.bold),
-                        ),
-                      ],
-                    ),
-                  ),
-                );
-              });
+                        SizedBox(
+                          height: 20,
+                        )
+                      ]);
+                    });
               }),
-    ));
+        ));
   }
 }
