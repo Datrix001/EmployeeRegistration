bcaaaabc82c6e43621d9a2260254f2aa1aede1ff
Datrix001 <mayankmoullisingh2004@gmail.com>
2025-03-03 16:27:37 +0530

    Finalized login ui

--- lib/styles/textField.dart
+++ lib/styles/textField.dart
@@ -9,11 +9,33 @@ class NormField {
 
   Widget normal() {
     return TextField(
+      style: AppText.body1Style(),
       decoration: InputDecoration(
         labelText: label,
         hintText: hint,
         hintStyle : AppText.body1Style(),
         labelStyle: AppText.body1Style(),
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(20),
+          borderSide: BorderSide(
+            color: Colors.white,
+            width: 2
+          )
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(20),
+          borderSide: BorderSide(
+            color: Colors.white,
+            width: 2
+          ),
+        ),
+        enabledBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(20),
+          borderSide: BorderSide(
+            color: Colors.white,
+            width: 2
+          ),
+        ),
       ),
     );
   }
