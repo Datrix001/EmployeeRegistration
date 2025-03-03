895e17b35d4787ec6df1592000bf08538865daca
Datrix001 <mayankmoullisingh2004@gmail.com>
2025-03-03 13:42:04 +0530

    Added basic layout for the login Page

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:notes/styles/Colors.dart';
+import 'package:notes/styles/buttons.dart';
 import 'package:notes/styles/font.dart';
 
 class Login extends StatefulWidget {
@@ -7,23 +8,81 @@ class Login extends StatefulWidget {
 
   @override
   State<Login> createState() => _LoginState();
-
 }
 
 class _LoginState extends State<Login> {
   @override
   Widget build(BuildContext context) {
-  //Changing classes
-  AppText.headerSize = 33;
+    //Changing classes
+    AppText.headerSize = 33;
 
     return Scaffold(
       backgroundColor: AppColors.backgroundColor,
       body: Column(
-        
         children: [
-          Spacer(),
-          Center(child: Text("Login",style: AppText.headerStyle(),),),
-          Center(child: Text("Login Back To Your Account And Resume Where You Left Off.",style: AppText.bodyStyle(), textAlign: TextAlign.center,))
+          SizedBox(
+            height: 100,
+          ),
+          Center(
+            child: Text(
+              "Login",
+              style: AppText.headerStyle(),
+            ),
+          ),
+          Center(
+              child: Text(
+            "Login Back To Your Account And Resume Where You Left Off.",
+            style: AppText.bodyStyle(),
+            textAlign: TextAlign.center,
+          )),
+          Text("Email Here"),
+          TextField(),
+          Text("Password"),
+          TextField(),
+          Text("Forgot Password"),
+          ElevatedButton(
+              onPressed: () {},
+              style: AppButton.ElevatedStyle,
+              child: Text(
+                "Login",
+                style: AppText.bodyStyle(),
+              )),
+          Text("Or Login With"),
+          ElevatedButton(
+              onPressed: () {},
+              child: Row(
+                children: [
+                  Image.asset(
+                    "assets/images/facebook.png",
+                    height: 20,
+                  ),
+                  SizedBox(
+                    width: 20,
+                  ),
+                  Text("Sign Up With Facebook")
+                ],
+              )),
+          ElevatedButton(
+              onPressed: () {},
+              child: Row(
+                children: [
+                  Image.asset(
+                    "assets/images/facebook.png",
+                    height: 20,
+                  ),
+                  SizedBox(
+                    width: 20,
+                  ),
+                  Text("Sign Up With Facebook")
+                ],
+              )),
+
+              Row(
+                children: [
+                  Text("Don't have an Account?"),
+                  TextButton(onPressed: (){}, child: Text("Sign Up Here"))
+                ],
+              )
         ],
       ),
     );
